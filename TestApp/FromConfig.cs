1b9ba269fe040014578d35533a3f34334962e112
Avinash <avinashsachan@live.in>
2019-12-31 07:55:19 +0530

    p1

---
+++ TestApp/FromConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace TestApp
+{
+    public static class FromConfig
+    {
+        public static string Server
+        { get { return ConfigurationManager.AppSettings["Server"].ToString(); } }
+
+        public static int Port
+        { get { return System.Convert.ToInt32(ConfigurationManager.AppSettings["Port"].ToString()); } }
+
+        public static string SystemID
+        { get { return ConfigurationManager.AppSettings["SystemID"].ToString(); } }
+
+        public static string Password
+        { get { return ConfigurationManager.AppSettings["Password"].ToString(); } }
+    }
+
+}
